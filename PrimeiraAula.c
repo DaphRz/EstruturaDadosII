cf566aee233ae6a6a7e70c17c1dd14414444cc2c
DaphRz <159149963+DaphRz@users.noreply.github.com>
2026-02-04 10:21:06 -0300

    terminar

--- PrimeiraAula.c
+++ PrimeiraAula.c
@@ -10,4 +10,22 @@ Programa em C que imprime "Olá, mundo." sem o uso de ; no código.
     }
 
 Escreva um TAD (Tipo Abstrato de Dados - um tipo definido pelo usuário - conjunto de elementos q contém propriedades iguais e conjunto de operações) que represente uma circunferência com as seguintes operações (funções):
+
 1. Criar uma Circunferência.
+
+    typedef struct {
+        int x, y;
+        float raio;
+    } circ;
+    
+    circ* criar_c(int x, int y, float raio){
+            circ *c = malloc((sizeof(circ)));
+            
+            (*c).x = x;
+            c->y = y;
+            c->raio = raio;
+            
+            return c;
+        }
+
+2. Destruir.
