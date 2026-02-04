3d6a2bf6d283f5a8f6af0fd9683bd29136b0bbc3
DaphRz <159149963+DaphRz@users.noreply.github.com>
2026-02-04 10:05:28 -0300

    terminar

---
+++ PrimeiraAula.c
@@ -0,0 +1,13 @@
+Programa em C que imprime "Olá, mundo." sem o uso de ; no código.
+
+    #include <stdio.h>
+    
+    int main()
+    {
+        if(printf("Hello World")){}
+    
+        return 0;
+    }
+
+Escreva um TAD (Tipo Abstrato de Dados - um tipo definido pelo usuário - conjunto de elementos q contém propriedades iguais e conjunto de operações) que represente uma circunferência com as seguintes operações (funções):
+1. Criar uma Circunferência.
