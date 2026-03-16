b871fcb9b5a58bee18fa34a7d138d516094b2180
DaphRz <159149963+DaphRz@users.noreply.github.com>
2026-03-16 08:53:06 -0300

    segunda aula

--- Arvore-B/b_tree.c
+++ Arvore-B/b_tree.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "b_tree.h"
 
-int main()
-{
-    printf("Hello World");
+void main()
+{    
+    FILE *dados = fopen("fonte.txt", "a+");
+    FILE *indice = fopen("indice.txt", "a+");
+    
+    char texto_linha[100];
+
+    arvB btree = criarArv();
+
+    if (dados == NULL){
+        printf("ERRO! O arquivo não foi aberto.\n");
+    }
+    else {
+        printf("O arquivo foi aberto com sucesso!\n");
+    }
+ 
+    while(fgets(texto_linha,100,dados) != NULL) {
+        printf("%s",texto_linha);
+    }
+    criarArv(dados, indice);
+
+    fclose(dados);
+    false(indice);
 
     return 0;
 }
+
+arvB *criarArv() {
+
+    arvB *raiz = (arvB*) malloc(sizeof(arvB));
+
+    if(raiz == NULL)
+        return NULL;
+
+    *raiz = (NO*) malloc(sizeof(NO));
+
+    if(*raiz == NULL)
+        return NULL;
+
+    (*raiz)->qtd = 0;
+    (*raiz)->eh_folha = 1;
+
+    for(int i = 0; i < M; i++)
+        (*raiz)->filhos[i] = NULL;
+
+    printf("Árvore B criada.\n");
+
+    return raiz;
+}
+
+int cadastrar(FILE *arvore, dado *elementos, NO *no);
+int pesquisar(FILE *arvore, int matricula);
+int gravar(FILE *arvore);
+int sair(FILE *arvore);
--- Arvore-B/b_tree.h
+++ Arvore-B/b_tree.h
@@ -1,14 +1,50 @@
-#define M 4
+#ifndef B_TREE_H
+#define B_TREE_H
+
+#include <stdio.h>
+
+#define M 3
+
+typedef struct _pagina {
+    int chave[M -1];
+    struct _pagina *filho[M];
+    int posicao[M-1];
+    int folha;
+} Pagina;
+
+typedef Pagina *ArvoreB;
+
+typedef struct dado {
+    int posicao;
+    int matricula;
+    char nome[50];
+    int telefone;
+} dado;
 
 typedef struct chave {
     int matricula;
-    NO* posicao;
+    int posicao;
 } chave;
 
 typedef struct NO {
-    NO* no;
     int qtd;
-    chave chaves[M];
-    NO* filhos[M+1];
+    chave chaves[M-1];
+    struct NO *filhos[M];
+    int eh_folha;
 } NO;
 
+typedef struct NO *arvB;
+
+arvB *criarArv();
+
+NO *criarNO();
+
+int cadastrar(FILE *arvore, dado *elementos, NO *no);
+int pesquisar(FILE *arvore, int matricula);
+int gravar(FILE *arvore);
+int sair(FILE *arvore);
+
+// FILE *data = fonte.tx
+// FILE *arvore = indice.txt
+
+#endif
